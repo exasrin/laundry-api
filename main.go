9ee10ec68fb133b99106a1179e63b21f9774acb9
asrin <asrinasinua@gmail.com>
2024-05-10 09:37:21 +0700

    [MODIFIED] connection database main func

--- main.go
+++ main.go
@@ -4,8 +4,16 @@ import (
 	"database/sql"
 	"fmt"
 	"go-api-enigma/config"
+
 	_ "github.com/lib/pq"
 )
+
+type Customer struct {
+	Id          string
+	Name        string
+	PhoneNumber string
+	Address     string
+}
 
 func main() {
 	cfg, error := config.NewConfig()
@@ -22,8 +30,27 @@ func main() {
 	)
 
 	db, error := sql.Open(cfg.DbConfig.Driver, dsn)
-	if error != nill {
+	if error != nil {
 		fmt.Println(error)
 	}
-	db.Exec("INSERT INTO")
+
+	Customer := Customer{
+		Id:          "1",
+		Name:        "Alffathir Rasyid Sulaiaman",
+		PhoneNumber: "081234567890",
+		Address:     "Jakarta",
+	}
+
+	_, error = db.Exec("INSERT INTO m_customer VALUES ($1, $2, $3, $4)",
+		Customer.Id,
+		Customer.Name,
+		Customer.PhoneNumber,
+		Customer.Address,
+	)
+
+	if error != nil {
+		fmt.Println(error)
+	}
+
+	fmt.Println("success inserting data")
 }
